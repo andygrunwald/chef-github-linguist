60e0350248667f9a32c967d8b752761edd34ce6b
Andy Grunwald <andygrunwald@gmail.com>
2013-04-29 21:36:17 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'github-linguist'
+maintainer       'Andy Grunwald'
+maintainer_email 'andygrunwald@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures github-linguist'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
