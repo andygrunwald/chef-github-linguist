afb7de7f2896eb6c2312ed4f4af58a7c4c5c6686
Andy Grunwald <andygrunwald@gmail.com>
2014-04-27 15:46:05 +0200

    Set correct year for copyright

--- recipes/source.rb
+++ recipes/source.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: github-linguist
 # Recipe:: source
 #
-# Copyright 2013, Andy Grunwald
+# Copyright 2013-2014, Andy Grunwald
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
